535e1b26730f27064cfbc31bda9790f342b016c1
alex <69579692+0xrivst@users.noreply.github.com>
2025-03-27 14:56:53 +0200

    8.4-8.5

--- part8/library/index.js
+++ part8/library/index.js
@@ -111,7 +111,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -120,7 +120,21 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author && args.genre)
+        return books.filter(
+          (book) =>
+            book.author === args.author && book.genres.includes(args.genre)
+        );
+
+      if (args.author)
+        return books.filter((book) => book.author === args.author);
+
+      if (args.genre)
+        return books.filter((book) => book.genres.includes(args.genre));
+
+      return books;
+    },
     allAuthors: () => authors,
   },
   Author: {
