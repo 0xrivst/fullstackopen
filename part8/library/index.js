402f35e5348bf6263b26a70fecab01620201e44b
alex <69579692+0xrivst@users.noreply.github.com>
2025-03-27 15:17:46 +0200

    8.6

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v4: uuid } = require("uuid");
 
 let authors = [
   {
@@ -114,6 +115,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+  }
 `;
 
 const resolvers = {
@@ -140,6 +150,20 @@ const resolvers = {
   Author: {
     bookCount: (root) =>
       books.filter((book) => book.author === root.name).length,
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+
+      // console.log(book.author);
+
+      if (!authors.find((author) => author.name === book.author)) {
+        authors = authors.concat({ name: book.author, id: uuid() });
+      }
+
+      return book;
+    },
   },
 };
 
